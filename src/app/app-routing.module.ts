c0656f7ffc02042bd0e756892bd5d2c1be7a19e5
bchincho <braianchincho@gmail.com>
2020-01-05 18:55:18 -0300

    lazy load

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,15 +10,15 @@ import { dashboardRoutes } from './dashboard/dashboard.routes';
 import { AuthGuardService } from './auth/auth-guard.service';
 
 
+
 const routes: Routes = [
 
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
     {
         path: '',
-        component: DashboardComponent,
-        children: dashboardRoutes,
-        canActivate: [AuthGuardService]
+        loadChildren:'./ingreso-egreso/ingreso-egreso.module#IngresoEgresoModule',
+        canLoad: [AuthGuardService]
     },
     { path: '**', redirectTo: '' }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,6 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { IngresoEgresoComponent } from './ingreso-egreso/ingreso-egreso.component';
 import { EstadisticaComponent } from './ingreso-egreso/estadistica/estadistica.component';
@@ -18,39 +16,33 @@ import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
-import { AngularFireAuthModule } from '@angular/fire/auth';
 import { environment } from 'src/environments/environment';
 import { StoreModule } from '@ngrx/store';
 import { appReducer } from './app.reducer';
 import { ChartsModule } from 'ng2-charts';
+
+
+// custom module
+import { AuthModule } from './auth/auth.module';
+import { SharedModule } from './shared/shared.module';
+import { IngresoEgresoModule } from './ingreso-egreso/ingreso-egreso.module';
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    RegisterComponent,
-    DashboardComponent,
-    IngresoEgresoComponent,
-    EstadisticaComponent,
-    DetalleComponent,
-    FooterComponent,
-    NavbarComponent,
-    SidebarComponent,
-    OrdenarPipe
+ 
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
-    ReactiveFormsModule,
+    IngresoEgresoModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
-    AngularFireAuthModule,
     StoreModule.forRoot(appReducer),
     StoreDevtoolsModule.instrument({
       maxAge: 25, // Retains last 25 states
       logOnly: environment.production, // Restrict extension to log-only mode
     }),
-    ChartsModule
+    AuthModule
   ],
   exports: [ ChartsModule],
   providers: [],
